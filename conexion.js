c15a0b3a986d0a8c5b50e0c0ce2c9492ed61ba14
Joseline Martinez <arl.martinez@duocuc.cl>
2025-05-26 20:10:55 -0400

    Ajustes Conexion

--- conexion.js
+++ conexion.js
@@ -9,7 +9,7 @@ const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASS || '',
-  database: process.env.DB_NAME || 'Ferremax',
+  database: process.env.DB_NAME || 'Ferremas',
   port: parseInt(process.env.DB_PORT) || 3306,
   waitForConnections: true,
   connectionLimit: 10,
